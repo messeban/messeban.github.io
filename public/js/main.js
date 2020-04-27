89b125c3a9cc5e6f90985eb95ccf64189550c490
Mourad Essebane <essebane.mourad@gmail.com>
2020-04-27 18:45:01 +0200

    Added Style

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000
 });
+
+$(function() {
+    $(document).scroll(function() {
+      var $nav = $("#thisNavbar");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    })
+  })
+
